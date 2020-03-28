0711adae180dac34711cdba74227ac7d9a61eef1
akumargs <mail.arvind85@gmail.com>
2020-03-28 14:23:06 +0530

    Pushing missed file

---
+++ family_tree/src/main/java/com/akgs/algo/familytree/common/exception/CommandFailedException.java
@@ -0,0 +1,7 @@
+package com.akgs.algo.familytree.common.exception;
+
+public class CommandFailedException extends Throwable {
+    public CommandFailedException(Throwable e){
+        super(e);
+    }
+}
